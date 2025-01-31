864bc604a793943c99784d1e1d1c702210268c72
Alex <91335015+SnowyAlex16@users.noreply.github.com>
2025-01-31 19:44:47 +0000

    attempt for download

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import axios from "axios"
+import download from "downloadjs"
+
 const menuIcon = document.querySelector('#menu-icon')
 const navLinks = document.querySelector('.nav-links')
 
@@ -9,4 +12,21 @@ menuIcon.onclick = () => {
 function toGraphicsYt(){
     const url = 'https://youtu.be/l84rPQ4s50o';
     window.open(url);
+}
+function toGraphicsYt(){
+    const url = 'https://youtu.be/l84rPQ4s50o';
+    window.open(url);
+}
+function toGraphicsYt(){
+    const url = 'https://youtu.be/l84rPQ4s50o';
+    window.open(url);
+}
+export const DownloadCV = async () => {
+    const url = `https://www.googleapis.com/drive/v3/files/${import.meta.env.VITE_DOCUMENT_ID}/export?key=${import.meta.env.VITE_API_KEY}`
+    const response = await axios.get(url, { responseType: 'blob', params: { 'mimeType': 'application/pdf' } })
+    try {
+        download(response.data, 'Alexandre Kouda.pdf', 'application/pdf')
+    } catch (error) {
+        console.debug(`Error getting file: ${error}`)
+    }
 }
