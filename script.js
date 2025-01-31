aa517d632e9f180905c55430dfe817df62c0d56b
Alex <91335015+SnowyAlex16@users.noreply.github.com>
2025-01-31 20:15:48 +0000

    Major Fix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const menuIcon = document.querySelector('#menu-icon')
 const navLinks = document.querySelector('.nav-links')
 
@@ -8,19 +6,7 @@ menuIcon.onclick = () => {
     navLinks.classList.toggle('active')
 }
 
-function DownloadCV() {
-    const url = `Alexandre Kouda.pdf`;
-    window.open(url);
-}
-function toGraphicsYt1(){
+function toGraphicsYt(){
     const url = 'https://youtu.be/l84rPQ4s50o';
-    window.open(url);
-}
-function toItch(){
-    const url = 'https://the-scoofy.itch.io/brewmaster-seige'
-    window.open(url)
-}
-function toCYt(){
-    const url = 'https://youtu.be/ieL8A9HKIfQ'
     window.open(url);
 }
