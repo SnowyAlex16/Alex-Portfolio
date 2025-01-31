3fd25fae47207ec82638d85892268683d2f98d05
Alex <91335015+SnowyAlex16@users.noreply.github.com>
2025-01-31 19:06:19 +0000

    try

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menuIcon = document.querySelector('#menu-icon')
 const navLinks = document.querySelector('.nav-links')
 
+
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active')
+}
+
+function toGraphicsYt(){
+    window.open('')
 }
