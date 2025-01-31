6e993440eb167df0b5fb9e8f6cc6989355e2efca
Alex <91335015+SnowyAlex16@users.noreply.github.com>
2025-01-31 20:32:06 +0000

    fix

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function toGraphicsYt(){
     const url = 'https://youtu.be/l84rPQ4s50o';
     window.open(url);
 }
+function toCV(){
+    const url = 'https://docs.google.com/document/d/18w2xyYNUsTJJupOg8Nsbd_FfKeeK_IYH/edit?usp=sharing&ouid=116188126036407737934&rtpof=true&sd=true';
+    window.open(url);
+}
+function toDiss(){
+    const url = 'https://drive.google.com/file/d/1okVDHRQdytqc1sTtHdhVvCZ2LE1suTuO/view?usp=sharing';
+    window.open(url);
+}
+function toCYt(){
+    const url = 'https://youtu.be/ieL8A9HKIfQ';
+    window.open(url);
+}
+function toItch(){
+    const url = 'https://the-scoofy.itch.io/brewmaster-seige';
+    window.open(url);
+}
